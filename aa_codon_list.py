b44f4f4617458d47f9d6ff0b7136c7d4ae2651e3
Mattis <mattisknulst@gmail.com>
2020-10-14 13:04:26 +0200

    Update aa_codon_list.py
    format editing

--- aa_codon_list.py
+++ aa_codon_list.py
@@ -1,16 +1,17 @@
-"""
-This was adapted from an incredibly elegant list comprehension found on http://www.petercollingridge.co.uk/tutorials/bioinformatics/codon-table/
-I wanted to see if I could adapt it to list codons given the amino acid, and it worked.
-"""
-nts = "tcag" #nucleotides in genetic code
-cods = [a + b + c for a in nts for b in nts for c in nts] #generates a list of all possible three letter combinations from nucleotides
-aas = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG' #string to assign amino acids to nucleotides
-cods_tabl = dict(zip(cods, aas)) # make a dictionary from codons and amino acids
-leu = ['tta', 'ttg', 'ctt', 'ctc', 'cta', 'ctg'] # hard coded list of codons for L for comparison
-print("the correct answer should be: ", "\n", leu) 
-rel = [(key, value) for (key, value) in cods_tabl.items() if 'L' in value] #extract all key-value pairs corresponding to string
+"""This was adapted from an incredibly elegant list comprehension found on
+http://www.petercollingridge.co.uk/tutorials/bioinformatics/codon-table/ I wanted to see if I could adapt it to list
+codons given the amino acid, and it worked. """
+nts = "tcag"  # nucleotides in genetic code
+cods = [a + b + c for a in nts for b in nts for c in
+        nts]  # generates a list of all possible three letter combinations from nucleotides
+aas = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'  # string to assign amino acids to nucleotides
+cods_tabl = dict(zip(cods, aas))  # make a dictionary from codons and amino acids
+leu = ['tta', 'ttg', 'ctt', 'ctc', 'cta', 'ctg']  # hard coded list of codons for L for comparison
+print("the correct answer should be: ", "\n", leu)
+rel = [(key, value) for (key, value) in cods_tabl.items() if
+       'L' in value]  # extract all key-value pairs corresponding to string
 answer = []
 for z, y in rel:
-    answer.append(z) #put all requested codons in answer
-if answer == leu: #test if the answer is correct
+    answer.append(z)  # put all requested codons in answer
+if answer == leu:  # test if the answer is correct
     print(answer, "\n", "yay you did it!!!")
